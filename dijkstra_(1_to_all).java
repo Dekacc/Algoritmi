2024172d9883434599daa76d8eaa971aee4b39be
David <dekacc@outlook.com>
2016-09-04 03:19:12 +0200

    Dijkstra's algorithm with prioQueue from 1 vertex to all.

--- dijkstra_(1_to_all).java
+++ dijkstra_(1_to_all).java
@@ -16,7 +16,7 @@ public class Main {
 			int V = Integer.parseInt(split1[0]);
 			int E = Integer.parseInt(split1[1]);
 			Vertex[] ver = new Vertex[V];
-			for(int i = 0; i < V; i++) ver[i] = new Vertex(i);
+			for(int i = 0; i < V; i++) ver[i] = new Vertex();
 			for(int i = 0; i < E; i++){
 				String line = br.readLine();
 				String[] split = line.split(" ");
@@ -30,26 +30,21 @@ public class Main {
 			//INPUT ENDS
 			
 			//DIJKSTRA
-			Vertex current = ver[start];
-			current.shortest = 0;
-			current.visited = true;
-			TreeSet<Vertex> pq = new TreeSet<>();
-			pq.add(current);
-			while(!pq.isEmpty()){
-				current = pq.pollFirst();
-				current.visited = true;
-				for (Edge e : current.edges) {
-					if (ver[e.dest].visited)
-						continue;
-					int length = current.shortest + e.length;
-					if (length < ver[e.dest].shortest) {
-						// update the placement in prioQueue
-						pq.remove(ver[e.dest]);						
-						ver[e.dest].shortest = length;
-						pq.add(ver[e.dest]);
-					}
-				}
-			}
+            		PriorityQueue<Pair> pq = new PriorityQueue<>();
+            		pq.add(new Pair(start, 0));
+            		while(!pq.isEmpty()){
+                		Pair current = pq.remove();
+                		if(current.distance <= ver[current.destination].shortest){
+                    			ver[current.destination].shortest = current.distance;
+                    			for(Edge e : ver[current.destination].edges){
+                        			int newDist = current.distance + e.length;
+                        			if(newDist < ver[e.dest].shortest){
+                            				pq.add(new Pair(e.dest, newDist));
+                            				ver[e.dest].shortest = newDist;
+                        			}
+                    			}
+                		}
+            		}
 
 			//PRINT
 			for(int i = 0; i < V; i++){
@@ -63,35 +58,27 @@ public class Main {
 	}
 }
 
+class Pair implements Comparable<Pair>{
+    	int destination;
+    	int distance;
+    	Pair(int dest, int dist){
+        	destination = dest;
+        	distance = dist;
+    	}
+
+    	@Override
+    	public int compareTo(Pair p) {
+        	return distance - p.distance;
+	}
+}
+
 class Vertex implements Comparable<Vertex> {
-	int index;
-	boolean visited;
 	int shortest;
 	ArrayList<Edge> edges;
 
-	Vertex(int i) {
-		visited = false;
-		index = i;
+	Vertex() {
 		shortest = Integer.MAX_VALUE;
 		edges = new ArrayList<>();
-	}
-
-	@Override
-	public int compareTo(Vertex o) {
-		//mora vaka za da ne se izbagira treeSetot
-		int dif = shortest - o.shortest;
-		if(dif == 0) return index - o.index;
-		return dif;
-	}
-
-	@Override
-	public int hashCode() {
-		return index;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		return index == ((Vertex) obj).index;
 	}
 }
 
