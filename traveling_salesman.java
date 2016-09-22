9acbda4519e4876e428c77b9c68f600bdb472ff2
David <dekacc@outlook.com>
2016-09-22 16:38:39 +0200

    dynamic recursive solution O(n^2 * 2^n)

--- traveling_salesman.java
+++ traveling_salesman.java
@@ -14,17 +14,29 @@ import java.util.Arrays;
 public class Costumes {
 	static int N;
 	static int[][] dp;
+	static int[][] next; //for path keeping (DELETE IF NO PATH IS ASKED, IT SPEEDS UP THE ALGORITHM)
 	static int[][] distances;
 	
     private static int solve(int bit, int pos){
         if(dp[pos][bit] != -1) return dp[pos][bit];
         
         int min = Integer.MAX_VALUE;
+        int minIndex = -1; //for path keeping (DELETE IF NO PATH IS ASKED, IT SPEEDS UP THE ALGORITHM)
+        
         for(int i = 1; i < N; i++){ //(starts with 1 because 0th index is always visited)
             if((bit & (1 << i)) == 0){
-                min = Math.min(min, distances[i][pos] + solve(bit | (1 << i), i));
+            	//min = Math.min(min, distances[i][pos] + solve(bit | (1 << i), i));
+            	
+            	//UNCOMENT THE PREVIOUS COMMENT, AND DELETE THIS IF NO PATH IS ASKED
+            	int dist = distances[i][pos] + solve(bit | (1 << i), i);
+                if(dist < min){
+                	min = dist;
+                	minIndex = i;
+                }
+                
             }
         }
+        next[pos][bit] = minIndex; //for path keeping (DELETE IF NO PATH IS ASKED, IT SPEEDS UP THE ALGORITHM)
         return dp[pos][bit] = min;
     }
 
@@ -32,6 +44,7 @@ public class Costumes {
     	N = x.length;
         int S = 1 << N;
         dp = new int[N][S];
+        next = new int[N][S];
         distances = new int[N][N];
         
         //fill distances matrix
@@ -52,12 +65,25 @@ public class Costumes {
     }
 
     public static void main(String[] args){
-//        int x[] = {166,866,171,523,614,533,728,262,77,29,730,828,273,704,959,149,630,928,657,69};
-//        int y[] = {19,981,324,868,493,564,834,559,23,168,674,894,538,436,251,240,412,994,2,69};
-//        long startTime = System.currentTimeMillis();
-//        System.out.println(minimumPath(x, y));
-//        long endTime   = System.currentTimeMillis();
-//        long totalTime = endTime - startTime;
-//        System.out.println(totalTime + " ms");
+        int x[] = {874,117,684,989,878};
+        int y[] = {878,929,484,798,210};
+        //long startTime = System.currentTimeMillis();
+        System.out.println(minimumPath(x, y));
+        //long endTime   = System.currentTimeMillis();
+        //long totalTime = endTime - startTime;
+        //System.out.println(totalTime + " ms");
+        
+        //PRINT PATH
+        int current = 0;
+        int bit = 1;
+        System.out.print(current);
+        while(true){
+        	System.out.print(" -> ");
+        	int n = next[current][bit];
+        	System.out.print(n);
+        	current = n;
+        	if(current == 0) break;
+        	bit = bit | (1<<current);
+        }
     }
 }
